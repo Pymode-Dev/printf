94630d64262f2184305196346caa90131857529b
Pymode-Dev <pymode000@gmail.com>
2023-06-27 01:24:49 -0700

    Fix some bugs

--- main.c
+++ main.c
@@ -2,12 +2,18 @@
 
 int main()
 {
-	_printf("Hello %c, %s", 'A', "World\n");
-	_printf("Let's try to printf a simple sentence.\n");
-	_printf("Hello world\n");
-	_printf("String: [%s]\n", "I am a string !");
-	_printf("Character:[%c]\n", 'H');
-	_printf("Length:[%d, %i]\n", 10, 10);
+	int l1, l2;
+
+	l1 = _printf("Let's try to printf a simple sentence.\n");
+	l2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", l1, l2);
+	printf("Length:[%d, %i]\n", l1, l2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	l1 = _printf("Percent:[%%]\n");
+	l2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", l1);
+	printf("Len:[%d]\n", l2);
 
 	return (0);
 }
